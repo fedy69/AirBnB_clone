96ada8efa18c5488d88b728021cb5ed80026a321
EyaZidi19 <91125384+EyaZidi19@users.noreply.github.com>
2022-03-06 16:58:42 -0500

    Update __init__.py

--- tests/models/__init__.py
+++ tests/models/__init__.py
@@ -2,7 +2,11 @@
 """The module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.place import Place
 from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
 storage = FileStorage()
 
 
