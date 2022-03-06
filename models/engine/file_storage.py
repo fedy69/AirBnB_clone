cf7939bf289b1cfcc1bfe86ce52898eee3037815
EyaZidi19 <3805@holbertonschool.com>
2022-03-06 06:34:36 -0800

    add the pop item

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,8 +13,15 @@ class Objects(dict):
     """The class object"""
 
     def __getitem__(self, key):
-        """get item"""
+        """the get item"""
         try:
             return super(Objects, self).__getitem__(key)
+        except Exception as e:
+            raise Exception("** no instance found **")
+
+    def pop(self, key):
+        """the pop item"""
+        try:
+            return super(Objects, self).pop(key)
         except Exception as e:
             raise Exception("** no instance found **")
