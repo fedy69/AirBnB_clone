caa5bb1c77148be95a2b60d01634f7b0aca72e1e
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-03-07 11:58:30 +0100

    Add classes. Validate id for update and destroy

--- models/state.py
+++ models/state.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python
-""" holds class State"""
+#!/usr/bin/python3
+'''Module for State class'''
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Representation of state """
+    """Inherits from BaseModel"""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
