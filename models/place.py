15cdcb27ab61c4444fd0b589a88a4e23d5ba862b
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-03-07 11:55:58 +0100

    Add classes

--- models/place.py
+++ models/place.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
-"""The class  Place"""
+#!/usr/bin/python3
+'''Module for Place class'''
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """The Representation of the Place """
+    """Inherits from BaseModel"""
     city_id = ""
     user_id = ""
     name = ""
@@ -16,7 +16,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        """class to initializes Place"""
-        super().__init__(*args, **kwargs)
